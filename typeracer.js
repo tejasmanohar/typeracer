5f30eb47679fe210c578054ec1092085e5d1d0f9
Tejas Manohar <me@tejasmanohar.com>
2014-12-27 01:34:37 -0600

    get text

--- typeracer.js
+++ typeracer.js
@@ -3,9 +3,9 @@ function getScript(url,success) {
   script.src=url;
   var head=document.getElementsByTagName('head')[0],
   done=false;script.onload=script.onreadystatechange=function(){
-      if(!done&&(!this.readyState||this.readyState=='loaded'||this.readyState=='complete')){
-          done=true;success();script.onload=script.onreadystatechange=null;head.removeChild(script);
-      }
+    if(!done&&(!this.readyState||this.readyState=='loaded'||this.readyState=='complete')){
+    done=true;success();script.onload=script.onreadystatechange=null;head.removeChild(script);
+    }
   };
   head.appendChild(script);
 }
@@ -13,3 +13,7 @@ function getScript(url,success) {
 getScript('http://code.jquery.com/jquery-latest.min.js',function(){
   return win();
 });
+
+function win() {
+  console.log($('div.nonHideableWords').text()); 
+}
