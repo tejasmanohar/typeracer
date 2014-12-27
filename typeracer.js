92ed6d776822a8e748ca65bd1092b7d899083f8a
Tejas Manohar <me@tejasmanohar.com>
2014-12-27 02:13:52 -0600

    2 many vars not issue

--- typeracer.js
+++ typeracer.js
@@ -32,8 +32,9 @@ var forEachDelay = function(delay, f, xs) {
 // Rek Scrubz
 function trollEveryoneElse() {
   var text = $('div.nonHideableWords').text();
+  var chars = text.length;
   var arrText = text.split(' ');
-  var timeRemaining = (((text.length/5)*60)/190)*1000;
+  var timeRemaining = (((chars/5)*60)/190)*1000;
   var delay = timeRemaining/arrLength;
   for (var i = 0; i < arrLength; i++) {
     forEachDelay(delay, function (text) {
